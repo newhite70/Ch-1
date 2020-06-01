09db425c27d1411efa639e17eb22a0432b7af5ab
Initial Commit <newhite7070@gmail.com>
2020-06-01 15:30:29 -0400

    Day 1

---
+++ Exercise/Numeric Types/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Numeric_Types
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int width;
+            int length;
+            string length_input;
+            string width_input;
+            
+
+            Console.WriteLine("Please provide the length of the rectangle:");
+            length_input = Console.ReadLine();
+            length = int.Parse(length_input);
+            Console.WriteLine("Please provide the width of the rectangle:");
+            width_input = Console.ReadLine();
+            width = int.Parse(width_input);
+            int area = (width * length);
+            Console.WriteLine(area);
+        }
+    }
+}
---
+++ More Numeric/More Numeric/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace More_Numeric
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int miles;
+            int gallons;
+            string miles_input;
+            string gallons_input;
+
+
+            Console.WriteLine("How many miles have you driven:");
+            miles_input = Console.ReadLine();
+            miles = int.Parse(miles_input);
+            Console.WriteLine("How many gallons of fuel have you used:");
+            gallons_input = Console.ReadLine();
+            gallons = int.Parse(gallons_input);
+            int mph = (miles / gallons);
+            Console.WriteLine(mph);
+        }
+    }
+}
